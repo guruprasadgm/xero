1f3d3009b7fdbd35bd97d045ae03b29a75f74d36
Guruprasad Mahale <Guruprasad.Mahale@airnz.co.nz>
2020-10-24 15:47:50 +1300

    Initial Commit

--- src/main/java/com/xero/Page/DashboardPage.java
+++ src/main/java/com/xero/Page/DashboardPage.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
